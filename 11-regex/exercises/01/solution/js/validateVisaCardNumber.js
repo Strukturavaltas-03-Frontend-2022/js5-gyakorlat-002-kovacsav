6e12ea5b3f0d7892f2243269d25482c92c6477c7
kovacsav <mezbogyo@gmail.com>
2022-09-15 15:36:36 +0200

    done

--- 11-regex/exercises/01/solution/js/validateVisaCardNumber.js
+++ 11-regex/exercises/01/solution/js/validateVisaCardNumber.js
@@ -1,1 +1,6 @@
+const validateVisaCardNumber = (cardNumber = "") => {
+  const cardNumberPattern = /^4(\d{12})?(\d{15})?$/;
+  return cardNumberPattern.test(cardNumber);
+};
+
 export default validateVisaCardNumber;
--- 11-regex/exercises/04/solution/js/validate.js
+++ 11-regex/exercises/04/solution/js/validate.js
@@ -1,1 +1,11 @@
+const roles = {
+  visa: /^4(\d{12})?(\d{15})?$/,
+  ip: /^((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[1-9])(\.(?!$)|$))((25[0-5]|(2[0-4]|1[0-9]|[1-9]|)[0-9])(\.(?!$)|$)){3}$/,
+  mac: /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/,
+};
+
+const validate = (stringToValidate, validatePattern) => {
+  return roles[validatePattern].test(stringToValidate);
+};
+
 export default validate;
